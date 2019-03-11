fd4e8bee54d68c09308eed76dee32f4597f983a8
gmartone <martoneg@lafayette.edu>
2019-03-11 18:02:01 -0400

    New Project, all views

---
+++ DyerC/src/main/java/com/msn/gabrielle/ui/MainPage.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2000-2017 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.msn.gabrielle.ui;
+
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dependency.HtmlImport;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.page.Viewport;
+import com.vaadin.flow.router.HighlightConditions;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouterLayout;
+import com.vaadin.flow.router.RouterLink;
+import com.vaadin.flow.server.InitialPageSettings;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.server.PageConfigurator;
+import com.msn.gabrielle.ui.views.categorieslist.CategoriesList;
+import com.msn.gabrielle.ui.views.reviewslist.ReviewsList;
+
+/**
+ * The main layout contains the header with the navigation buttons, and the
+ * child views below that.
+ */
+@Route(value = "", layout = MainLayout.class)
+public class MainPage extends Div
+        implements RouterLayout{
+	
+	private Button studentButton;
+	private Button alumniButton;
+	private Button employeeButton;
+
+	
+    public MainPage() {
+    	studentButton = new Button("Student");
+    	add(studentButton);
+    	studentButton.addClickListener( e-> {
+    		studentButton.getUI().ifPresent(ui -> ui.navigate("studentPage"));
+    	});
+    	
+    	alumniButton = new Button("Alumni");
+    	add(alumniButton);
+    	alumniButton.addClickListener( e-> {
+    		alumniButton.getUI().ifPresent(ui -> ui.navigate("alumniPage"));
+    	});
+    	
+    	employeeButton = new Button("Employee");
+    	add(employeeButton);
+    	employeeButton.addClickListener( e-> {
+    		employeeButton.getUI().ifPresent(ui -> ui.navigate("employeePage"));
+    	});
+    	
+    }
+   
+}
