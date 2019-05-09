ae40d50b87ca5ee92cc8edc11ba586fec803472d
cencije <cencije@lafayette.edu>
2019-05-09 11:24:00 -0400

    Small fixes.

--- DyerC/src/main/java/com/msn/gabrielle/ui/views/Employee/SQLProjectEmp.java
+++ DyerC/src/main/java/com/msn/gabrielle/ui/views/Employee/SQLProjectEmp.java
@@ -6,6 +6,8 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 
+import com.msn.gabrielle.backend.Projects;
+
 public class SQLProjectEmp {
 
 	
@@ -37,7 +39,7 @@ public class SQLProjectEmp {
 			statementInsertProject.executeUpdate(sqlInsertProject);
 			statementInsertProject.close();
 			c.close();
-			System.out.println("Successful Insertion to TABLE_EVENTS_MASTER");
+			System.out.println("Successful Insertion to TABLE_PROJECT_INDEX");
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.err.println(e.getClass().getName()+": "+e.getMessage());
@@ -64,12 +66,57 @@ public class SQLProjectEmp {
 				statementInsertSkill.close();
 			}
 			c.close();
-			System.out.println("Successful Insertion to TABLE_EVENTS_MASTER");
+			System.out.println("Successful Insertion to TABLE_PROJECT_SKILLS");
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.err.println(e.getClass().getName()+": "+e.getMessage());
 			System.exit(0);
-		}
+		}	
+	}
+	
+	public ArrayList<Projects> loadProjects() {
 		
+		String eID = "", pTitle = "", pStart = "", pEnd = "", 
+				   pLocation = "", pDesc = "", pPaid = "", pProp = "", pPosted = "";
+		ArrayList<Projects> listProjects = new ArrayList<Projects>();
+		try {
+			Class.forName("org.postgresql.Driver");
+			Connection c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "PostgresMall");
+			System.out.println("-----------------------------------------------------------------");
+			System.out.println("Loading Projects from TABLE_PROJECT_INDEX");
+
+			Statement statementLoadProjects = c.createStatement();
+			
+			String sqlLoadProjects = "SELECT * FROM TABLE_PROJECT_INDEX;";
+			
+			ResultSet rsProjects = statementLoadProjects.executeQuery(sqlLoadProjects);
+
+			while(rsProjects.next()) {
+				
+				eID	   = rsProjects.getString(1);
+				pTitle  = rsProjects.getString(2);
+				pStart = rsProjects.getString(3); 
+				pEnd = rsProjects.getString(4); 
+				pLocation = rsProjects.getString(5); 
+				pDesc  = rsProjects.getString(6); 
+				pPaid  = rsProjects.getString(7); 
+				pProp  = rsProjects.getString(8); 
+				pPosted  = rsProjects.getString(9); 
+				
+				// Create New Project and Add to List
+				// listProjects.add(new Projects(pTitle,pStart,pEnd,pLocation,pDesc,pPaid,pProp,pPosted));
+			}
+			statementLoadProjects.executeUpdate(sqlLoadProjects);
+			statementLoadProjects.close();
+
+			c.close();
+			System.out.println("Successful Loading to TABLE_PROJECT_INDEX");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getClass().getName()+": "+e.getMessage());
+			System.exit(0);
+		}	
+		return listProjects;
 	}
 }
